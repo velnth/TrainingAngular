f3071cf569361896c5e542b5d4c1c4fb2cd10224
velnth <zafiramarvella1588@gmail.com>
2025-09-13 16:22:50 +0700

    hmc

---
+++ src/app/category-product/category-product.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-product',
+  imports: [],
+  templateUrl: './category-product.html',
+  styleUrl: './category-product.css'
+})
+export class CategoryProduct {
+
+}
